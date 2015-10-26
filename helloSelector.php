2966c911b7b362b79614b72a34abc88e2ae31c3c
1203926 <1203926@rgu.ac.uk>
2015-10-26 14:15:49 +0000

    hellosewlect

---
+++ helloSelector.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+
+
+<body>
+<p>
+    <?php
+    echo        "Hello World";
+
+    echo "Where would you like to say hello?" /n /n;
+
+
+
+    ?>
+</p>
+</body>
+</html>
